0c381e4239b970a65cdbaf4832663da501125a24
vxcarson <vxcarson@gamil.com>
2017-12-13 01:27:54 +0000

    app

--- pokiTrainer/public/javascripts/app.js
+++ pokiTrainer/public/javascripts/app.js
@@ -1,13 +1,18 @@
-angular.module('myApp', []).
-  controller('myController', ['$scope', '$http',
-                              function($scope, $http) {
-    $http.get('/user/profile')
-        .success(function(data, status, headers, config) {
-      $scope.user = data;
-      $scope.error = "";
-    }).
-    error(function(data, status, headers, config) {
-      $scope.user = {};
-      $scope.error = data;
-    });
-  }]);
+var myApp = angular.module("myApp",["firebase"]);
+myApp.controller("mainCtrl", ["$scope", "$firebaseArray",
+ function($scope, $firebaseArray) {
+
+   $scope.username = 'Ashley Ketchup'
+/*
+   var ref = firebase.database().ref().child("messages");
+   $scope.chats = $firebaseArray(ref);
+   $scope.update = function(user) {
+       var newmessage = {from:user.name || "anonymous",body:user.chat};
+       console.log(newmessage);
+       $scope.chats.$add(newmessage);
+       user.chat = "";
+*/
+
+   }
+ }
+]);

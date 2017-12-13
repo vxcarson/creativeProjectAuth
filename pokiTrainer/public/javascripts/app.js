6cf417e179fc9d320b8e6dcf3c9bf3cc63edcf24
Philip Horton <philip.horton@att.net>
2017-12-13 02:13:16 +0000

    change to book report

--- pokiTrainer/public/javascripts/app.js
+++ pokiTrainer/public/javascripts/app.js
@@ -1,18 +1,50 @@
-var myApp = angular.module("myApp",["firebase"]);
-myApp.controller("mainCtrl", ["$scope", "$firebaseArray",
- function($scope, $firebaseArray) {
+$(document).ready(function(){
+  $("#postReport").click(function(){
+      var myobj = {Title:$("#title").val(),Report:$("#report").val()};
+      jobj = JSON.stringify(myobj);
+      var titleName = $("#title").val();
+      $("#json").text("Successfully inserted " + titleName + "!");
+      $("#reports").html("<ul></ul>");
+      var url = "/report";
+      $.ajax({
+           url:url,
+           type: "POST",
+           data: jobj,
+           contentType: "application/json; charset=utf-8",
+           success: function(data,textStatus) {
+                 console.log(data);
+           }
+      })
+  });
 
-   $scope.username = 'Ashley Ketchup'
-/*
-   var ref = firebase.database().ref().child("messages");
-   $scope.chats = $firebaseArray(ref);
-   $scope.update = function(user) {
-       var newmessage = {from:user.name || "anonymous",body:user.chat};
-       console.log(newmessage);
-       $scope.chats.$add(newmessage);
-       user.chat = "";
-*/
 
-   }
- }
-]);
+  $("#getReport").click(function() {
+    $.getJSON('/report', function(data) {
+      console.log(data);
+      $("#json").text("");
+      var everything = "<ul>";
+      for(var report in data) {
+        rpt = data[report];
+        everything += "<li> Title: " + rpt.Title + " -- Report: " + rpt.Report + "</li>";
+      }
+      everything += "</ul>";
+      $("#reports").html(everything);
+    })
+  });
+
+  
+  $('#deleteReports').click(function() {
+    var url = "/report";
+    $("#json").text("");
+    $.ajax({
+      url: url,
+      type: "DELETE",
+      contentType: "application/json; charset=utf-8",
+      success: function(data, textStatus) {
+        console.log("Cleared");
+        $("#reports").html("<ul></ul>");
+      }
+    })
+  });
+
+});
